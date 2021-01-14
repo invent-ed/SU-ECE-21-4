ec885f4be30b6e154cc2cbf3df13e1cb526c6a9c
CalienS <72034432+CalienS@users.noreply.github.com>
2021-01-14 14:15:01 -0800

    The logger in sift keypoint generator file.

--- recognition/ConcreteClass/SiftKeypointsGenerator.py
+++ recognition/ConcreteClass/SiftKeypointsGenerator.py
@@ -7,17 +7,20 @@ from ConcreteClass.SiftKeypoints import SiftKeypoints
 class SiftKeypointsGenerator(KeypointsGenerator):
 
     def __init__(self, config, maskGenerator=None):
+        logging.info("Initializing keypoint generator")
         self.config = config
         self.maskGenerator = maskGenerator
         self.sift = cv2.xfeatures2d.SIFT_create()
         self.create_sift_dir_if_not_exist()
 
     def create_sift_dir_if_not_exist(self):
+        logging.info("Creating Sift directory")
         sift_dir = self.config.get("Keypoints.directory")
         if not os.path.exists(sift_dir):
             os.makedirs(sift_dir)
 
     def generate_keypoints_if_not_exist(self, imageObj):
+        logging.info("Generating sift keypoints")
         maskObj = self.get_mask_if_mask_generator_exists(imageObj)
         kps_path = self.generate_kps_path(imageObj.filename)
         if not os.path.isfile(kps_path):
@@ -26,6 +29,7 @@ class SiftKeypointsGenerator(KeypointsGenerator):
         return SiftKeypoints(kps_path, maskObj)
 
     def get_mask_if_mask_generator_exists(self, imageObj):
+        logging.info("Getting mask in the keypoint generator.")
         if self.maskGenerator is not None:
             maskObj = self.maskGenerator.generate_mask_if_not_exist(imageObj)
         else:
@@ -33,11 +37,13 @@ class SiftKeypointsGenerator(KeypointsGenerator):
         return maskObj
 
     def generate_kps_path(self, filename):
+        logging.info("Generating kp path.")
         kp_dir = self.config.get("Keypoints.directory")
         kp_ext = self.config.get("Keypoints.file_extension")
         return os.path.abspath(kp_dir).replace("\\", "/") + "/" + filename + kp_ext
 
     def generate_keypoints(self, imageObj, maskObj=None):
+        logging.info("Generating keypoints.")
         if maskObj is not None:
             keypoints, descriptors = self.sift.detectAndCompute(imageObj.image, maskObj.mask)
         else:
